005ca416d62f396d47731d9ccec515478530829d
RAria <pcomeara@comcast.net>
2019-03-27 16:46:50 -0700

    commit

---
+++ MathAndComparisonOperatorsExcercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperatorsExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRateString = Console.ReadLine();
+            int hourlyRate = Convert.ToInt32(hourlyRateString);
+          
+            Console.WriteLine("Hours Worked per week?");
+            string = Console.ReadLine();
+        }
+    }
+}
