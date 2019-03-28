38f79b9cd12546d2dce3960e3c8531ef6bc8042e
RAria <pcomeara@comcast.net>
2019-03-27 19:08:53 -0700

    Hi there.

---
+++ MathAndComparisonOperatorsExcercise/ex67/ex67/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperatorsExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRateString = Console.ReadLine();
+            int hourlyRate = Convert.ToInt32(hourlyRateString);
+            Console.WriteLine("Hours Worked per week?");
+            string weekHoursString = Console.ReadLine();
+            int weekHours = Convert.ToInt16(weekHoursString);
+            int annualSalary1 = weekHours * 52 * hourlyRate;
+
+            Console.WriteLine("Person 12");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRateString2 = Console.ReadLine();
+            int hourlyRate2 = Convert.ToInt32(hourlyRateString);
+            Console.WriteLine("Hours Worked per week?");
+            string weekHours2String = Console.ReadLine();
+            int weekHours2 = Convert.ToInt16(weekHoursString2);
+            int annualSalary2 = weekHours2 * 52 * hourlyRate2;
+
+            Console.WriteLine("Does Person 1 make more money than Person 2");
+            bool trueOrFalse = annualSalary1 > annualSalary2;
+            Console.WriteLine(trueOrFalse);
+            Console.Read();
+
+
+        }
+    }
+}
--- MathAndComparisonOperatorsExcercise/ex67/ex67/Properties/AssemblyInfo.cs
+++ MathAndComparisonOperatorsExcercise/ex67/ex67/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MathAndComparisonOperatorsExcercise")]
+[assembly: AssemblyTitle("ex67")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MathAndComparisonOperatorsExcercise")]
+[assembly: AssemblyProduct("ex67")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5a6c616e-c55c-4860-8403-0fae49e4c343")]
+[assembly: Guid("29462baa-4e94-4942-b165-4859cf088459")]
 
 // Version information for an assembly consists of the following four values:
 //
