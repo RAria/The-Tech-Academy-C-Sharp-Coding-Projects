4e3c86e069b79a5ee1e36b64d0c7e24f85335fa0
RAria <pcomeara@comcast.net>
2019-03-27 19:22:37 -0700

    final changes

--- MathAndComparisonOperatorsExcercise/ex67/ex67/Program.cs
+++ MathAndComparisonOperatorsExcercise/ex67/ex67/Program.cs
@@ -18,16 +18,16 @@ namespace MathAndComparisonOperatorsExcercise
             int hourlyRate = Convert.ToInt32(hourlyRateString);
             Console.WriteLine("Hours Worked per week?");
             string weekHoursString = Console.ReadLine();
-            int weekHours = Convert.ToInt16(weekHoursString);
+            int weekHours = Convert.ToInt32(weekHoursString);
             int annualSalary1 = weekHours * 52 * hourlyRate;
 
-            Console.WriteLine("Person 12");
+            Console.WriteLine("Person 2");
             Console.WriteLine("Hourly Rate?");
             string hourlyRateString2 = Console.ReadLine();
-            int hourlyRate2 = Convert.ToInt32(hourlyRateString);
+            int hourlyRate2 = Convert.ToInt32(hourlyRateString2);
             Console.WriteLine("Hours Worked per week?");
-            string weekHours2String = Console.ReadLine();
-            int weekHours2 = Convert.ToInt16(weekHoursString2);
+            string weekHoursString2 = Console.ReadLine();
+            int weekHours2 = Convert.ToInt32(weekHoursString2);
             int annualSalary2 = weekHours2 * 52 * hourlyRate2;
 
             Console.WriteLine("Does Person 1 make more money than Person 2");
