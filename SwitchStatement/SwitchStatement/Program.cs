1d9e13a9a8e1c079fee0d614056c23055dfd7dc3
RAria <pcomeara@comcast.net>
2019-03-28 17:08:31 -0700

    do while loops

---
+++ SwitchStatement/SwitchStatement/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed =number == 55;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 25:
+                        Console.WriteLine("You guessed 25. Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 55:
+                        Console.WriteLine("You guessed 55. That is correct!");
+                        isGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+
+            }
+
+            while (!isGuessed);
+            Console.Read();
+        }
+    }
+}
