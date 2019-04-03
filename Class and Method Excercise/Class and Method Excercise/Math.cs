88e85957ad5661bbf0c43043e09d383a25e80a04
RAria <pcomeara@comcast.net>
2019-04-03 14:55:22 -0700

    method excercise

---
+++ Class and Method Excercise/Class and Method Excercise/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_Excercise
+{//A class which contains math operations which has two parameters of integers and return an integer.
+    public class Math
+    {
+        //method for addition.
+        public int Sum(int variable1, int variable2)
+        {
+
+            return variable1 + variable2;
+        }
+        //method for multiplication
+        public int Multiply(int variable1, int variable2)
+        {
+
+            return variable1 * variable2;
+        }
+        //method for substraction
+        public int Substract(int variable1, int variable2)
+        {
+            return variable1 - variable2;
+
+        }
+
+    }
+
+
+}
---
+++ Class and Method Excercise/Class and Method Excercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_Excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The program asks the user for two integers input and takes the input and pass it to method's parameters as arguments and returns the result.
+            Console.WriteLine("Please enter two whole numbers for math operation");
+            Console.WriteLine(" Enter first number");
+            //display for input
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            //call methods of the class Math
+            Math math = new Math();
+            int sum = math.Sum(num1, num2);
+            int multiply = math.Multiply(num1, num2);
+            int substract = math.Substract(num1, num2);
+
+            //display the returned value by methods
+            Console.WriteLine("\n\tHere is the result for addition: "+sum);
+            Console.WriteLine("\n\tHere is the result for multiplication: " + multiply);
+            Console.WriteLine("\n\tHere is the result for substraction: " + substract);
+            Console.ReadLine();
+        }
+    }
+}
