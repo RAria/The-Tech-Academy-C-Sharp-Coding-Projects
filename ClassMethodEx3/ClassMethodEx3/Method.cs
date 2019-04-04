445f0ec481ccf7fa76bcda3df2022cfa5c86ad8f
RAria <pcomeara@comcast.net>
2019-04-03 18:40:55 -0700

    Ex3

---
+++ ClassMethodEx3/ClassMethodEx3/Method.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodEx3
+{
+    //A class with 3 methods which take one argument of different data types and do math operations and return the result as integer.
+    public class Method
+    {
+        //Method takes argument of integer data type.
+        public int sum(int var1)
+        {
+           return  var1 + var1;
+        }
+
+        //Method takes argument of decimal datat ype.
+        public int sum(decimal var2)
+        {
+            return Convert.ToInt32(var2 * var2);
+
+        }
+
+        //Method takes argument of string data type.
+        public int sum(string var3)
+        {
+            int var = int.Parse(var3);
+
+            return var - var;
+
+        }
+    }
+}
---
+++ ClassMethodEx3/ClassMethodEx3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodEx3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            Method method = new Method();
+
+            //call methods within the class and pass an argument.
+            int sum = method.sum(var1 : 12);
+            int power = method.sum(var2 : 12.50m);
+            int substract = method.sum(var3 : "4");
+
+            //Display the return value.
+            Console.WriteLine(sum);
+            Console.WriteLine(power);
+            Console.WriteLine(substract);
+
+            Console.ReadLine();
+        }
+    }
+}
