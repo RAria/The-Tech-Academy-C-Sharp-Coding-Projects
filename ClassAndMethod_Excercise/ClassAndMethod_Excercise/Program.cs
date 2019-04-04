19cb284772c078ecb6bcbfb0cd7c0be8d2d4c0f9
RAria <pcomeara@comcast.net>
2019-04-03 17:37:53 -0700

    methods and excercise 1

---
+++ ClassAndMethod_Excercise/ClassAndMethod_Excercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod_Excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            VoidMethod voidMethod = new VoidMethod();
+            //call the method by passing arguments
+            voidMethod.method(4, 5);
+            // call the method by specifying names of the parameters.
+            voidMethod.method(var1 : 6, var2:8 );
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ ClassAndMethod_Excercise/ClassAndMethod_Excercise/VoidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod_Excercise
+{//A class with method that has two parameters. The method within the class will receive two intigers and do a math power operation and display the second integer.
+ //The assignment did not ask to return the result of the operation, I will leave it that way.
+    public class VoidMethod
+    {
+        public void method(int var1, int var2)
+        {
+            int power = var1 * var1;
+            Console.WriteLine(var2);
+        }
+    }
+}
