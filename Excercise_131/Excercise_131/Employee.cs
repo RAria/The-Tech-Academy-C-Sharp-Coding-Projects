64e26808cb4006f0427420bbe5559fc7794df95c
RAria <pcomeara@comcast.net>
2019-04-09 13:10:25 -0700

    excercise 131

---
+++ Excercise_131/Excercise_131/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_131
+{//class and its property list with generic type.
+    public class Employee<T>
+    {
+        public List<T> things { get; set; }
+    }
+}
---
+++ Excercise_131/Excercise_131/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//object instantiation of the employee class with a string data type.
+            Employee<string> employee = new Employee<string>();
+            employee.things = new List<string>() {"A","B","C","D","E" };
+
+            //object instantiation of the employee class with an integer data type.
+            Employee<int> integers = new Employee<int>();
+            integers.things = new List<int>() {1,2,3,4,5};
+
+            //loops to print the stored values.
+            foreach (string search in employee.things)
+            {
+                Console.WriteLine(search);
+                
+            }
+
+            foreach (int search in integers.things)
+            {
+                Console.WriteLine(search);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
