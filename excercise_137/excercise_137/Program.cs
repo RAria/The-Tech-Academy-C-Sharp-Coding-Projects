ad482d086007f481cba74f620eabe1ec0f542024
RAria <pcomeara@comcast.net>
2019-04-09 16:45:13 -0700

    excercise 137

---
+++ excercise_137/excercise_137/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excercise_137
+{
+    class Program
+    {//instantiation of struct and assigning value to the object.
+        static void Main(string[] args)
+        {
+            Number object1 = new Number();
+            object1.Amount = 5;
+
+            //print the value.
+            Console.WriteLine(object1.Amount);
+            Console.ReadLine();
+        }
+        
+    }
+}
