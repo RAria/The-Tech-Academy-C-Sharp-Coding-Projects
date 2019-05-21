c8a5c52bbcaf41762e3b9a12892c538423df933e
RAria <pcomeara@comcast.net>
2019-05-21 15:16:27 -0700

    Car Insurance Project

---
+++ Car Insurance Project/Car Insurance Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Car_Insurance_Project.Startup))]
+namespace Car_Insurance_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ CarInsurance_DF/CarInsurance_DF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarInsurance_DF.MvcApplication" Language="C#" %>
